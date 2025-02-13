51e13ea16492e2b5c6d2ace403af250512fc6519
AbdullahNoumanN599 <109980559+AbdullahNoumanN599@users.noreply.github.com>
2025-02-13 22:33:32 +0300

    Created the first model of the UI
    This is the first model of the UI code currently it isn't very useful as a calculator and the UI can look cleaner. i will improve upon it once we get the functions sorted out

---
+++ temp.py
@@ -0,0 +1,87 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+import tkinter as tk
+root = tk.Tk()
+root.geometry("800x600")
+CalcScreen = tk.Entry(root, text = "" , font= ('Times New Roman', 25, 'bold'))
+CalcScreen.grid(row=1, column= 2)
+#the screen displaying all inputs is created and the font times new roman is chosen because i think its nice
+
+def ButtonsWritingText(TXT):
+    CalcScreen.insert(tk.END, TXT)
+    #we create a function with a conditional variable TXT for our buttons
+def ButtonDeletingText():
+    current_text = CalcScreen.get()
+    if len(current_text)> 0: #if statement is to ensure we dont get an error for trying to delete whats not already there
+        CalcScreen.delete(len(current_text)-1,tk.END)
+def ButtonDeletingAllText():
+    CalcScreen.delete(0,tk.END)
+ #we create two functions to serve as del buttons.
+
+class button:
+    def __init__(self,root,NameInString,rowx,columny):
+        self.NameInString= NameInString 
+        self.button = tk.Button(root, text = self.NameInString, command= lambda: ButtonsWritingText(NameInString))
+        self.button.grid(row=rowx, column=columny, sticky="nsew")
+    
+
+#a class for our buttons to make the process faster,cleaner and easier
+
+button0 = button(root, "0", 12, 2)
+Button1 =button(root, "1", 11, 1)
+button2 = button(root, "2", 11, 2)
+button3=  button(root, "3", 11, 3)
+button4 = button(root, "4", 10, 1)
+button5 = button(root, "5", 10, 2)
+button6 = button(root, "6", 10, 3)
+button7 = button(root, "7", 9, 1)
+button8 = button(root, "8", 9, 2)
+button9 = button(root, "9", 9, 3)
+buttonDiv = button(root, "/", 11, 4)
+buttonmult =  button(root, "x", 10, 4)
+buttonadd = button(root, "+", 9, 4)
+buttonleftpara = button(root, "(", 12, 1)
+buttonrightpara = button(root, ")", 12, 3)
+buttonsub = button(root, "-", 10, 4)
+buttonsin= button(root, "sin", 8, 1)
+buttoncos = button(root, "cos", 8, 2)
+buttontan = button(root, "tan", 8, 3)
+buttonarcsin = button(root, "arcsin", 7, 1)
+buttonarccos = button(root, "arccos", 7, 2)
+buttonarctan = button(root, "arctan", 7, 3)
+buttonsqr = button(root, "sqr", 6, 1)
+buttonsqrroot = button(root, "sqr root", 6, 2)
+buttonequal = button(root, "=", 6,3)
+ButtonDel = tk.Button(root, text="Del",command= ButtonDeletingText)
+ButtonDel.grid(row=8,column=4,sticky="nsew")
+ButtonAC = tk.Button(root, text="AC",command=ButtonDeletingAllText)
+ButtonAC.grid(row=7,column=4,sticky="nsew")
+#some buttons are exceptions in function and thus werent created as
+#an object of the button class
+
+for i in range (6, 13):
+    root.grid_rowconfigure(i, weight = 1)
+
+
+for j in range(1,5):
+    root.grid_columnconfigure(j, weight = 1)
+
+#two for loops to ensure that the buttons aren't too small and too spread out
+#for loops make this simpler and easier
+
+
+
+
+
+
+
+
+
+
+
+
+tk.mainloop()
